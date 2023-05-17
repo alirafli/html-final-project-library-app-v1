e6aa73ad88652f9320a0522f3142abbab4af0a47
alirafli <mrafli1212@gmail.com>
2023-05-17 09:35:41 +0700

    initial commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Mohammad Ali Rafli'; // TODO: replace this
+
+const studentId = 'FE4234561'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://euphonious-parfait-424c01.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
